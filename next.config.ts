61c34c7a62a0513894ac9b15cdee6351502448cb
Wilson Medeiros <wilsonmdrs@gmail.com>
2025-10-17 19:06:00 +0100

    fix: webpack

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
-import path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    config.resolve.alias["@public"] = path.resolve(__dirname, "public");
-    return config;
-  },
-  turbopack: {
-    resolveAlias: {
-      "@/*": "./src/*",
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Footer } from "@/components/footer";
-import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
 import { AboutSection } from "@/components/sections/about";
 import { HeroSection } from "@/components/sections/hero";
 import { ResumeSection } from "@/components/sections/resume";
